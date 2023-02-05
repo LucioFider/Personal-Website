eb432857bcbcf13051fe3d747118de64f6053b35
LucioFider <fidelislucio404@gmail.com>
2023-02-06 09:54:29 +1000

    2nd-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import styles from "./style";
+import {
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  FeedbackCard,
+  Footer,
+  GetStarted,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+} from "./components";
+
 function App() {
   return (
-    <div>
-      <h1>New Site</h1>
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>navbar</div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats business Billing card Deal testimonials Clients CTA Footer
+        </div>
+      </div>
     </div>
   );
 }
