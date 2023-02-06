d48ba5b7c0fe83b5dd57bbc1f3eb826bed9edc8e
LucioFider <fidelislucio404@gmail.com>
2023-02-06 15:10:10 +1000

    5th-commit

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,7 +1,59 @@
 import React from "react";
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div
+    className={`flex flex-row p-6 rounded-[20px] ${
+      index !== features.length - 1 ? "mb-6" : "mb-0"
+    } feature-card`}
+  >
+    <div
+      className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+    >
+      <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1">
+        {content}
+      </p>
+    </div>
+  </div>
+);
 
 const Business = () => {
-  return <div>Business</div>;
+  return (
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Empowering Your Vision, <br className="sm:block hidden" />
+          with Technical Expertise.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          This highlights the client's aspirations for their online presence and
+          the developer's ability to turn those aspirations into a tangible
+          reality. By partnering together, we can create a mutually beneficial
+          outcome that not only meets the client's needs but also surpasses
+          their expectations. The client brings their vision, and the developer
+          brings their technical know-how to produce a website or online
+          platform that is a true representation of the client's goals and
+          values. This collaboration empowers both parties to achieve success
+          and create something truly remarkable.
+        </p>
+        <Button styles="mt-10" />
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Business;
